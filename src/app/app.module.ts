ff02e83ed5ffeb742e6030f4618cc723fcd4e5ed
garbrwn <garybrwn88@googlemail.com>
2021-10-28 03:22:22 +0100

    Grids

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatGridListModule} from '@angular/material/grid-list';
+
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+  img: string;
+}
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+
+
+
+export class HomeComponent implements OnInit {
+
+  tiles: Tile[] = [
+    {text: 'One', cols: 3, rows: 1, color: 'lightblue', img: ''},
+    {text: 'Two', cols: 1, rows: 2, color: '#cdcdcd', img: ''},
+    {text: 'Three', cols: 1, rows: 1, color: 'lightpink', img: ''},
+    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1', img: './assets/img/BrokenRobot.png'},
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
